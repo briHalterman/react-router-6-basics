1619be690d9be6f5218265e7aea952fd94d830e7
Brittany Halterman <halterma@gmail.com>
2025-03-24 17:14:21 -0400

    Display 404 error on routes not found

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Courses from './components/Courses';
 import HTML from './components/courses/HTML';
 import CSS from './components/courses/CSS';
 import JavaScript from './components/courses/JavaScript';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="css" element={<CSS />} />
           <Route path="javascript" element={<JavaScript />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
