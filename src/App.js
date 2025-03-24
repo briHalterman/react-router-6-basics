2a8450eea6fc526b60a96159e59b1dfb15af95c3
Brittany Halterman <halterma@gmail.com>
2025-03-24 12:30:57 -0400

    Create descendant routes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="teachers" element={<Teachers />} />
-        <Route path="courses" element={<Courses />} />
+        <Route path="courses/*" element={<Courses />} />
       </Routes>
     </div>
   );
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
-      <h2>Courses</h2> 
+      <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='#'>HTML</a></li>
-        <li><a href='#'>CSS</a></li>
-        <li><a href='#'>JavaScript</a></li>
+        <li>
+          <NavLink to="/courses/html">HTML</NavLink>
+        </li>
+        <li>
+          <NavLink to="/courses/css">CSS</NavLink>
+        </li>
+        <li>
+          <NavLink to="/courses/javascript">JavaScript</NavLink>
+        </li>
       </ul>
     </div>
+    <Routes>
+      <Route path="html" element={<HTML />} />
+      <Route path="css" element={<CSS />} />
+      <Route path="javascript" element={<JavaScript />} />
+    </Routes>
   </div>
 );
 
